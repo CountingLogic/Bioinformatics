b3139da6d7e4db5dabadb94225da9f72ea41d059
SagarAI <sagarmalhotra0594@gmail.com>
2018-01-10 15:15:39 +0100

    Contribution

---
+++ git_new.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+startdate=2018-01-10
+enddate=2018-01-20
+
+sDateTs=`date -j -f "%Y-%m-%d" $startdate "+%s"`
+eDateTs=`date -j -f "%Y-%m-%d" $enddate "+%s"`
+dateTs=$sDateTs
+offset=86400
+
+while [ "$dateTs" -le "$eDateTs" ]
+do
+  date=`date -j -f "%s" $dateTs "+%Y-%m-%d"`
+  rand=$(((RANDOM % 5) + 1))
+        for j in $(seq 1 $rand)
+        do
+            git add .
+            git commit --date="$date" -m "Contribution" --allow-empty 
+            printf '%s\n' $date
+            dateTs=$(($dateTs+$offset))
+        done
+done
+
+git push -u origin master
